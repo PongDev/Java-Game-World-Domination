21ed932ab1b7ac57be7920bac85d34ca257af02e
PongDev <pongwiwat.dev@outlook.com>
2021-04-13 13:31:39 +0700

    Update gui/GameText.java

--- src/gui/GameText.java
+++ src/gui/GameText.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import utility.ResourceManager;
 
@@ -10,6 +11,11 @@ public class GameText extends Label {
 		Font font = Font.loadFont(ResourceManager.getFontResourceStream(), fontSize);
 		this.setFont(font);
 		this.setText(text);
+	}
+
+	public GameText(String text, double fontSize, Color color) {
+		this(text, fontSize);
+		this.setTextFill(color);
 	}
 
 }
