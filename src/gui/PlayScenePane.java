f7631bb6f79cc4bb7345f7520dc6e35a65b79324
Rada Wimolkhajornkitti <fainrada@gmail.com>
2021-05-18 19:33:52 +0700

    Update WaveManager

--- src/gui/PlayScenePane.java
+++ src/gui/PlayScenePane.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.util.Date;
 import config.Config;
 import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
@@ -8,19 +9,35 @@ import sound.SoundManager;
 import update.Updatable;
 import utility.ResourceManager.SceneResource;
 import utility.ResourceManager.SoundResource;
+import utility.WaveManager;
 
 public class PlayScenePane extends StackPane implements Updatable {
 
-	GameText pauseText;
+	private GameText waveText;
+	private GameText pauseText;
 
 	public PlayScenePane() {
+		waveText = new GameText("Wave " + Integer.toString(WaveManager.getWave()), Config.SCREEN_H / 9);
+		waveText.setAlignment(Pos.CENTER);
+		waveText.setTranslateY(-Config.SCREEN_H / 3);
+		waveText.setVisible(false);
+
 		pauseText = new GameText(Config.PAUSE_MESSAGE, Config.SCREEN_H / 5);
 		pauseText.setAlignment(Pos.CENTER);
 		pauseText.setVisible(false);
-		this.getChildren().addAll(GameState.getGameMap(), pauseText);
+		this.getChildren().addAll(GameState.getGameMap(), waveText, pauseText);
 	}
 
 	public void update() {
+		if (WaveManager.isDisplayWaveText == true) {
+			waveText.setText("Wave " + Integer.toString(WaveManager.getWave()));
+			waveText.setVisible(true);
+			if ((new Date()).getTime() - WaveManager.time > 2000) {
+				WaveManager.isDisplayWaveText = false;
+				waveText.setVisible(false);
+			}
+		}
+
 		if (SoundManager.getCurrentSoundResource() != SoundResource.PLAYING) {
 			SoundManager.setCurrentSound(SoundResource.PLAYING);
 		}
--- src/utility/WaveManager.java
+++ src/utility/WaveManager.java
@@ -1,15 +1,22 @@
 package utility;
 
+import java.util.Date;
+
 import character.Enemy;
 import config.Config;
+import gui.PlayScenePane;
+import javafx.scene.control.Label;
 import logic.GameState;
 import utility.ResourceManager.ImageResource;
+import utility.ResourceManager.SceneResource;
 import weapon.Gun;
 
 public class WaveManager {
 	private static int wave = 0;
 	public static int enemyPerWave = 0;
 	public static int enemyDied = 0;
+	public static long time = 0;
+	public static boolean isDisplayWaveText = false;
 
 	public static void startNewWave() {
 		wave += 1;
@@ -18,7 +25,9 @@ public class WaveManager {
 		for (int i = 1; i <= enemyPerWave; i++) {
 			testSpawnEnemy(i);
 		}
-		System.out.println("start wave " + enemyPerWave);
+		Logger.log("start wave " + wave);
+		isDisplayWaveText = true;
+		time = (new Date()).getTime();
 	}
 
 	public static void testSpawnEnemy(int i) {
@@ -32,7 +41,7 @@ public class WaveManager {
 				new Position((int) (Config.TILE_W * 2 * i), (GameState.getMapHeight() * Config.TILE_H) / 2));
 	}
 
-	public static int getwave() {
+	public static int getWave() {
 		return wave;
 	}
 }
