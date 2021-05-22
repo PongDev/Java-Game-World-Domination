59c9e1bb57209c87f819db12fa0a2c62966f6062
PongDev <pongwiwat.dev@outlook.com>
2021-05-23 01:43:58 +0700

    Add Java Doc On Package render

--- src/render/RenderManager.java
+++ src/render/RenderManager.java
@@ -4,14 +4,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * Render Manager: Management Render System
+ */
 public class RenderManager {
 
+	/**
+	 * Array List Contain Current In-Game Renderable Object
+	 */
 	private static ArrayList<Renderable> renderableObject = new ArrayList<Renderable>();
+
+	/**
+	 * Comparator For Sort ZIndex
+	 */
 	private static Comparator<Renderable> renderableComparator = (Renderable obj1, Renderable obj2) -> {
 		return (obj1.getZ() > obj2.getZ()) ? 1 : -1;
 	};
+
+	/**
+	 * Is renderableObject Currently Sort
+	 */
 	private static boolean isSort = true;
 
+	/**
+	 * Update renderableObject Array List Remove Destroyed Object
+	 */
 	private static void updateRenderableObject() {
 		for (int i = renderableObject.size() - 1; i >= 0; i--) {
 			if (renderableObject.get(i).isDestroyed()) {
@@ -20,6 +37,9 @@ public class RenderManager {
 		}
 	}
 
+	/**
+	 * Render Renderable Object In renderableObject Array List
+	 */
 	public static void render() {
 		updateRenderableObject();
 		if (!isSort) {
@@ -33,6 +53,9 @@ public class RenderManager {
 		}
 	}
 
+	/**
+	 * Add Renderable Object To renderableObject Array List
+	 */
 	public static void add(Renderable obj) {
 		isSort = false;
 		renderableObject.add(obj);
--- src/render/Renderable.java
+++ src/render/Renderable.java
@@ -1,13 +1,28 @@
 package render;
 
+/**
+ * Renderable Interface
+ */
 public interface Renderable {
 
+	/**
+	 * Function To Render
+	 */
 	public void render();
 
+	/**
+	 * Is Allow Render
+	 */
 	public boolean isAllowRender();
 
+	/**
+	 * Is Destroyed
+	 */
 	public boolean isDestroyed();
 
+	/**
+	 * Get ZIndex
+	 */
 	public int getZ();
 
 }
