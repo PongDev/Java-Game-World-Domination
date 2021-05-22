da12b5f9df26f131e200fb9ad11fb3edc36987d9
PongDev <pongwiwat.dev@outlook.com>
2021-05-23 02:42:07 +0700

    Add Java Doc On Package sound

--- src/sound/SoundManager.java
+++ src/sound/SoundManager.java
@@ -6,10 +6,22 @@ import utility.Logger;
 import utility.ResourceManager;
 import utility.ResourceManager.SoundResource;
 
+/**
+ * Sound Manager: Management Sound System
+ */
 public class SoundManager {
 
+	/**
+	 * Background Music SoundResource
+	 */
 	private static SoundResource bgmSoundResource;
 
+	/**
+	 * Set Current Background Music With Specific Volume
+	 * 
+	 * @param sound  Background Music SoundResource
+	 * @param volume Sound Volume
+	 */
 	public static void setCurrentBGM(SoundResource sound, double volume) {
 		if (bgmSoundResource != null) {
 			ResourceManager.getSound(bgmSoundResource).stop();
@@ -21,19 +33,38 @@ public class SoundManager {
 		Logger.log("Playing Sound " + bgmSoundResource.name());
 	}
 
+	/**
+	 * Set Current Background Music With Configured Sound Volume
+	 * 
+	 * @param sound Background Music SoundResource
+	 */
 	public static void setCurrentBGM(SoundResource sound) {
 		setCurrentBGM(sound, Config.SOUND_VOLUME);
 	}
 
+	/**
+	 * Play Sound Effect With Specific Volume
+	 * 
+	 * @param sound       Sound Effect SoundResource
+	 * @param soundVolume Sound Volume
+	 */
 	public static void playSoundEffect(SoundResource sound, double soundVolume) {
 		ResourceManager.getSound(sound).setVolume(soundVolume);
 		ResourceManager.getSound(sound).play();
 	}
 
+	/**
+	 * Play Sound Effect With Configured Sound Volume
+	 * 
+	 * @param sound Sound Effect SoundResource
+	 */
 	public static void playSoundEffect(SoundResource sound) {
 		playSoundEffect(sound, Config.SOUND_VOLUME);
 	}
 
+	/**
+	 * Get Current Background Music SoundResource
+	 */
 	public static SoundResource getCurrentBGMSoundResource() {
 		return bgmSoundResource;
 	}
