af6aa482854f98b0fe41ca437621b3d03db940a0
PongDev <pongwiwat.dev@outlook.com>
2021-05-23 11:41:41 +0700

    Add Java Doc On Package update

--- src/update/Updatable.java
+++ src/update/Updatable.java
@@ -1,9 +1,18 @@
 package update;
 
+/**
+ * Updatable Interface
+ */
 public interface Updatable {
 
+	/**
+	 * Function To Process Update
+	 */
 	public abstract void update();
 
+	/**
+	 * Is UpdateManager Should Remove Object From updatableObject
+	 */
 	public abstract boolean isRemoveFromUpdate();
 
 }
--- src/update/UpdateManager.java
+++ src/update/UpdateManager.java
@@ -2,11 +2,24 @@ package update;
 
 import java.util.ArrayList;
 
+/**
+ * Update Manager: Management Update Of Updatable Object
+ */
 public class UpdateManager {
 
+	/**
+	 * Array List Contain Updatable Object
+	 */
 	private static ArrayList<Updatable> updatableObject = new ArrayList<Updatable>();
+
+	/**
+	 * Array List Contain Updatable Object Waiting For Add Into updatableObject
+	 */
 	private static ArrayList<Updatable> updatableObjectQueue = new ArrayList<Updatable>();
 
+	/**
+	 * Update updatableObject Array List - Remove Item Should Remove From Update
+	 */
 	private static void updateUpdatableObject() {
 		for (int i = updatableObject.size() - 1; i >= 0; i--) {
 			if (updatableObject.get(i).isRemoveFromUpdate()) {
@@ -15,6 +28,10 @@ public class UpdateManager {
 		}
 	}
 
+	/**
+	 * Move Object In updatableObjectQueue To updatableObject Then Update Object In
+	 * updatableObject
+	 */
 	public static void update() {
 		for (Updatable obj : updatableObjectQueue) {
 			updatableObject.add(obj);
@@ -26,6 +43,9 @@ public class UpdateManager {
 		}
 	}
 
+	/**
+	 * Add Object To updatableObjectQueue
+	 */
 	public static void add(Updatable obj) {
 		updatableObjectQueue.add(obj);
 	}
